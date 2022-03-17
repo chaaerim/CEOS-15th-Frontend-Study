0a854502c7f65925cd04324a7675d93ecb46cde5
chaaerim <cofla411@ewhain.net>
2022-03-17 21:34:08 +0900

    toDoCount doneToDoCount 메소드 추가

--- js/todo.js
+++ js/todo.js
@@ -1,9 +1,25 @@
 const toDoInput = document.querySelector('input.toDoInput');
-const toDoNum = document.getElementsByClassName('toDoNum');
-const doneNum = document.getElementsByClassName('doneNum');
-const submitForm = document.getElementsByClassName('submitForm');
+const toDoNum = document.querySelector('.toDoNum');
+const doneNum = document.querySelector('.doneNum');
+const submitForm = document.querySelector('form');
 
 let toDos = [];
+
+// todo 카운팅
+function toDoCount() {
+  const toDoLeft = toDos.filter(
+    (newToDoObj) => newToDoObj.isCompleted === false
+  );
+  toDoNum.innerHTML = `(${toDoLeft.length})`;
+}
+
+// done todo 카운팅
+function doneToDoCount() {
+  const doneLeft = toDos.filter(
+    (newToDoObj) => newToDoObj.isCompleted === true
+  );
+  doneNum.innerHTML = `(${doneLeft.length})`;
+}
 
 //+버튼을 클릭하거나 엔터를 칠 때 실행
 function handleToDoSubmit(event) {
@@ -15,14 +31,20 @@ function handleToDoSubmit(event) {
   const newToDoObj = {
     text: newToDo,
     id: Date.now(),
+    isCompleted: false,
   };
   toDos.push(newToDoObj);
 
   // input창 비우기
   toDoInput.value = '';
 
-  paintToDo(newToDoObj);
-  saveToDos();
+  //paintToDo(newToDoObj);
+  toDoCount();
+  console.log(toDos);
+  //saveToDos();
 }
 
 submitForm.addEventListener('submit', handleToDoSubmit);
+
+//입력 받은 todo를 To Do 밑에 보여주기
+function paintToDo(newToDo) {}
